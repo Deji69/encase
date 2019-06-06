7a1a6432f2536da1eacc8a938bf461986e850834
Deji69 <the_zone@hotmail.co.uk>
2019-06-06 22:48:10 +0100

    Code cleanup

--- src/Encase/Functional/Collection.php
+++ src/Encase/Functional/Collection.php
@@ -64,16 +64,6 @@ class Collection extends Value
 	}
 
 	/**
-	 * Check if the collection is empty.
-	 *
-	 * @return bool
-	 */
-	public function isEmpty(): bool
-	{
-		return empty($this->value);
-	}
-
-	/**
 	 * Serialise the items as an array.
 	 *
 	 * @return static
@@ -99,36 +89,6 @@ class Collection extends Value
 		$this->value[] = $item;
 		return $this;
 	}
-
-	/**
-	 * Handles chainable method calls to Functional functions.
-	 * Returns $this if the method does not mutate or returns a new value.
-	 * Otherwise, a new Value instance is returned containing the value.
-	 *
-	 * @param  string  $method
-	 * @param  array   $parameters
-	 * @return static|$this
-	 */
-	/*public function __call($method, $params = [])
-	{
-		// Call the Functional function.
-		$result = $this->callFunctionalMethod($this->value, $method, $params);
-
-		// If the function returns an unmutated copy of its input, we'll return
-		// this instance to allow chaining.
-		if ($this->isMethodTapped($method)) {
-			return $this;
-		}
-
-		// If the function returns a mutated copy of its input, we'll return it
-		// wrapped in a new Value instance to allow chaining.
-		if ($this->isMethodAMutator($method) && !($result instanceof self)) {
-			return new self($result);
-		}
-
-		// For totally new values being returned, return it without wrapping.
-		return $result;
-	}*/
 
 	/**
 	 * Create a new collection instance.
