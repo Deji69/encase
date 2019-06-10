d8a6a0f1606978b12ba8e94eaddcc2dab5996efb
Deji69 <the_zone@hotmail.co.uk>
2019-06-10 03:03:47 +0100

    Remove redundant test class code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,38 +10,4 @@ abstract class TestCase extends BaseTestCase
 	{
 		Mockery::close();
 	}
-
-	protected static function it(string $message, ...$args)
-	{
-		if (count($args) == 1) {
-			self::assertTrue($args[0], $message);
-		} else {
-			do {
-				switch ($args[1]) {
-				case 'equals':
-				case 'must equal':
-					self::assertEquals($args[2], $args[0], $message);
-					break;
-				case 'not equals':
-				case 'must not equal':
-					self::assertNotEquals($args[2], $args[0], $message);
-					break;
-				case 'is':
-				case 'must be':
-					self::assertSame($args[2], $args[0], $message);
-					break;
-				case 'is not':
-				case 'must not be':
-					self::assertNotSame($args[2], $args[0], $message);
-					break;
-				}
-				array_splice($args, 0, 3);
-			} while (!empty($args));
-		}
-	}
-
-	protected static function assert(...$args)
-	{
-		self::it('', ...$args);
-	}
 }
