6ac83803b0233b4b0630a89fc35cd4891aedf081
Deji69 <the_zone@hotmail.co.uk>
2019-11-17 03:50:42 +0000

    Update Functional trait docblock

--- src/Encase/Functional/Traits/Functional.php
+++ src/Encase/Functional/Traits/Functional.php
@@ -7,18 +7,25 @@ use function Encase\Functional\isType;
  * Proxies method calls to \Encase\Functional functions.
  *
  * @method mixed  apply(callable $function)
- * @method $this  each(callable $function)
- * @method self   map(callable $function, bool $preserveKeys = false)
- * @method self   slice(?int $begin, int $end = null)
- * @method array|false  find(mixed $predOrValue, int $offset)
+ * @method self   concat(mixed ...$values)
  * @method int    count()
+ * @method $this  each(callable $function)
+ * @method self   fill($value, int $length = null)
+ * @method array|false  find(mixed $predOrValue, int $offset)
+ * @method mixed  first()
  * @method bool   isType(string|array $type)
+ * @method string join(?string $separator = ',', string $lastSeparator = null)
+ * @method mixed  last()
+ * @method self   map(callable $function, bool $preserveKeys = false)
  * @method mixed  pop()
  * @method mixed  shift()
  * @method int    size()
+ * @method self   slice(?int $begin, int $end = null)
  * @method array  split(string $separator = '', int $limit = null)
  * @method string type()
+ * @method array  union(...$arrayish)
  * @method self   unique(bool $keepKeyed = false, int $sortFlags = \SORT_REGULAR)
+ * @method self   values()
  */
 trait Functional
 {
