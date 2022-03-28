09fd783cb72b0ccb451bac87775c23f9878c95cd
Deji <the_zone@hotmail.co.uk>
2022-03-28 23:41:18 +0100

    Fix Type merge

--- src/Encase/Functional/type.php
+++ src/Encase/Functional/type.php
@@ -15,6 +15,9 @@ namespace Encase\Functional;
  */
 class Type
 {
+	const SCALAR_TYPES = [
+		'bool', 'float', 'int', 'string'
+	];
 	const TYPES = [
 		'array', 'bool', 'float', 'int', 'null', 'object', 'resource', 'string'
 	];
@@ -27,8 +30,17 @@ class Type
 
 	public function __construct(string $type, string $class = null)
 	{
-		$this->type = \in_array($type, self::TYPES, true) ? $type : null;
-		$this->class = $this->type === 'object' ? $class : null;
+		if (\in_array($type, self::TYPES, true)) {
+			$this->type = $type;
+			$this->class = $this->type === 'object' ? $class : null;
+		} else {
+			$this->type = 'object';
+			$this->class = $type;
+		}
+
+		if (!empty($this->class) && $this->class[0] === '\\') {
+			$this->class = \substr($this->class, 1);
+		}
 	}
 
 	/**
@@ -43,24 +55,28 @@ class Type
 	}
 
 	/**
-	 * Determine if the two Type objects represent the same type.
-	 * If the type is an object, also checks they are the same class.
-	 * If either type is unknown, returns FALSE.
+	 * Checks whether the Type object represents the given type/class, or that
+	 * they are equivalent to another Type objects type/class.
 	 *
-	 * @param  Type $type
+	 * @param  Type|string $type `Type`, or string representing the type.
+	 * @param  string|null $class If `$type` is `'object'`, this can set the class.
 	 * @return bool
 	 */
-	public function equals(Type $type)
+	public function equals($type, string $class = null)
 	{
-		if (!isset($this->type, $type->type)) {
+		if ($type instanceof $type) {
+			$class = $type->class;
+			$type = $type->type;
+		}
+		if (empty($this->type) || empty($type)) {
 			return false;
 		}
-		return $this->type === $type->type
-		    && $this->class === $type->class;
+		return $this->type === $type
+		    && $this->class === $class;
 	}
 
 	/**
-	 * Determine if `$var` is an instance of this Type.
+	 * Check if `$var` is an instance of this Type.
 	 *
 	 * Returns TRUE if `typeOf($var)` is the same as the type this Type
 	 * represents. If they are both objects, uses `instanceof` to also check if
@@ -69,7 +85,7 @@ class Type
 	 * @param  mixed $var
 	 * @return bool
 	 */
-	public function is($var)
+	public function check($var)
 	{
 		if (typeOf($var) === $this->type) {
 			if ($this->type === 'object' && $this->class !== null) {
@@ -78,6 +94,50 @@ class Type
 			return true;
 		}
 		return false;
+	}
+
+	public static function annotate($var): string
+	{
+		$type = typeOf($var);
+
+		if (\in_array($type, self::SCALAR_TYPES)) {
+			if ($type === 'string') {
+				$var = '\''.\addcslashes($var, '\'').'\'';
+			}
+			return "$type($var)";
+		} elseif ($type === 'object') {
+			return "$type(".\get_class($var).")";
+		} elseif ($type === 'array') {
+			if (\count($var) > 6) {
+				$parts = \array_slice($var, 0, 3, true);
+				$parts = \array_merge($parts, ['...'], \array_slice($var, -3, null, true));
+			} else {
+				$parts = $var;
+			}
+
+			$isSeq = isSequentialArray($parts);
+			$n = 0;
+
+			foreach ($parts as $key => &$part) {
+				$partType = typeOf($part);
+				$v = \in_array($partType, self::SCALAR_TYPES)
+					? (string)$part
+					: self::annotate($part);
+
+				if (++$n === 4 && $part === '...') {
+				} elseif ($partType === 'string') {
+					$v = '\''.\addcslashes($v, '\'').'\'';
+				}
+
+				if (\is_string($key)) {
+					$key = "'$key'";
+				}
+
+				$part = $isSeq ? $v : "$key => $v";
+			}
+			return '['.\implode(', ', $parts).']';
+		}
+		return "$type";
 	}
 
 	/**
@@ -107,9 +167,9 @@ class Type
 	/**
 	 * Create a new type using the name of the called static method.
 	 *
-	 * Convenience wrapper around `new Type((string)$type, ...$arguments)`.
+	 * Convenience wrapper around `new Type((string)$typeName, ...$arguments)`.
 	 *
-	 * @param  string $type
+	 * @param  string $typeName
 	 * @param  array $arguments [string $class]
 	 * @return self
 	 *
@@ -123,8 +183,8 @@ class Type
 	 * Type::object('My\OwnClass')
 	 * ```
 	 */
-	public static function __callStatic($type, $arguments): Type
+	public static function __callStatic($typeName, $arguments): Type
 	{
-		return new self((string)$type, ...$arguments);
+		return new self((string)$typeName, ...$arguments);
 	}
 }
